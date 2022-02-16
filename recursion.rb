dddeed6d90da1f5ac5fb479034536c5c6200d2ef
App Academy Student <email@email.com>
2022-02-16 15:15:39 -0500

    mergesort

--- recursion.rb
+++ recursion.rb
@@ -62,3 +62,34 @@ def bin_search(arr, target)
   # return nil
 end
 
+def merge_sort(arr)
+  middle =arr.length/2
+  left = arr[0...middle]
+  right = arr[middle...arr.length]
+  return [] if arr.length == 0
+  return [arr[0]] if arr.length == 1
+  sorted_left = merge_sort(left)
+  sorted_right = merge_sort(right)
+  merge(sorted_left,sorted_right)
+end
+
+def merge(left, right)
+  sorted = []
+  while left.length > 0 && right.length > 0
+    if left[0] > right[0]
+      sorted << right[0]
+      right.shift
+    else
+      sorted << left[0]
+      left.shift
+    end
+  end
+  if !left.length == 0
+    sorted.concat(left)
+  else
+    sorted.concat(right)
+  end
+  return sorted
+end
+
+p merge_sort([3, 7, 8, 1, 6, 2, 4])
